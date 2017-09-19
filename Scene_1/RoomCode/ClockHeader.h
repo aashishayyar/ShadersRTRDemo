ff4d5b93e2d57eec7ea287625bba8a52a37cf526
Yogesh Deshmukh <yogeshdeshmukh@Yogesh-Deshmukhs-MacBook-Pro.local>
2017-09-19 08:56:38 +0530

    Room code

---
+++ Scene_1/RoomCode/ClockHeader.h
@@ -0,0 +1,68 @@
+#pragma once
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+#define IDT_TIMER_SECOND 1
+#define IDT_TIMER_MINUTE 2
+#define IDT_TIMER_HOUR 3
+
+float gfClockOuterLength = 2.3f;
+float gfClockLength = 2.0f;
+float gfClockStepMin = 1.7f;
+float gfClockStepMax = 1.9f;
+float gfClockSecondHandRadious = 1.5f;
+float gfClockMinuteHandRadious = 1.3f;
+float gfClockHourHandRadious = 1.0f;
+float PI = 3.14159f;
+int giCirclePoints = 360;
+float gfSecondAngle = 0.0f;
+float gfMinuteAngle = 2.0f;
+float gfHourAngle = 1.0f;
+int giClockNumber = 0;
+float gfNumberMinLengthX = 0.15f;
+float gfNumberMaxLengthX = 0.3f;
+float gfNumberMinLengthY = 0.15f;
+float gfNumberMaxLengthY = 0.3f;
+float gfNumberOneLengthX = 0.1f;
+float gfNumberOneLengthY = 0.1f;
+float gfNumberSevenLengthX = 0.075f;
+float gfNumberSevenLengthY = 0.1f;
+float gfNumberFourLengthX = 0.1f;
+float gfNumberFourLengthY = 0.1f;
+
+float gfNumberTwelveDistanceFromOriginX = -0.1f;
+float gfNumberTwelveDistanceFromOriginY = 1.5f;
+
+float gfNumberOneDistanceFromOriginX = 0.9f;
+float gfNumberOneDistanceFromOriginY = 1.5f;
+
+float gfNumberTwoDistanceFromOriginX = 1.5f;
+float gfNumberTwoDistanceFromOriginY = 1.0f;
+
+float gfNumberThreeDistanceFromOriginX = 1.5f;
+float gfNumberThreeDistanceFromOriginY = 1.0f;
+
+float gfNumberFourDistanceFromOriginX = 1.5f;
+float gfNumberFourDistanceFromOriginY = -0.8f;
+
+float gfNumberFiveDistanceFromOriginX = 1.0f;
+float gfNumberFiveDistanceFromOriginY = -1.2f;
+
+float gfNumberSixDistanceFromOriginX = 0.1f;
+float gfNumberSixDistanceFromOriginY = -1.2f;
+
+float gfNumberSevenDistanceFromOriginX = -1.0f;
+float gfNumberSevenDistanceFromOriginY = -1.2f;
+
+float gfNumberEigthDistanceFromOriginX = -1.4f;
+float gfNumberEightDistanceFromOriginY = -0.8f;
+
+float gfNumberNineDistanceFromOriginX = -1.4f;
+float gfNumberNineDistanceFromOriginY = 0.1f;
+
+float gfNumberTenDistanceFromOriginX = -1.5f;
+float gfNumberTenDistanceFromOriginY = 1.0f;
+
+float gfNumberElevenDistanceFromOriginX = -1.0f;
+float gfNumberElevenDistanceFromOriginY = 1.5f;
+
+float gfTwoDigitDistance = 0.15;
---
+++ Scene_1/RoomCode/Matrix.h
@@ -0,0 +1,61 @@
+int giSevenSgment[] = { 0,0,0,0,0,0,0 };
+
+float gfWidthOfLine = 0.075f;
+float gfHeightOfLine = 0.075f;
+float gfCommonXDistance = (gfWidthOfLine * 4);
+float gfCommonYDistance = (gfHeightOfLine * 4);
+
+
+/*FINALIZE THE LEFT SIDE OF THE 6th LINE*
+* FOR FINALISING LOCATION OF NEXT CHARACTER
+* VARYING IRRESPECTIVE OF ANY POSITION
+*/
+
+float gfCurrentX = 0.0f;
+float gfCurrentY = 0.0f;
+float gfCurrentZ = 0.0f;
+
+float gfLineStartingY = -3.0f;	//ARRANGE THAT VALUE LIKE N FROM ANIMATED INDIA
+float gfLinePositionZ = 0.0f;	//THAT SHOULD BE RANDOM DEPTH VALUE GENERATED FOR EACH LINE
+
+
+
+float gfMatrixCharRed = 0.0f;
+float gfMatrixCharGreen = 1.0f;
+float gfMatrixCharBlue = 0.0f;
+
+int giNumberOfCharOnX = 100;
+int giNumberOfCharOnY = 100;
+int giTempNumberOfCharacterOnY;
+int giTempNumberOfCharacterOnX;
+
+float gfWidthOfRoom;
+float gfHeightOfRoom;
+
+
+DWORD gdwSpeedOfWallChange = 100;
+
+int *piRandomWallNumbers = NULL;
+int *piRandomLineNumbers = NULL;
+
+DWORD dwSavedTickCount = 0;
+DWORD dwGetTickCount = 0;
+DWORD dwDisplayedFirstWall = 0;
+
+DWORD dwMaximumCharaFromWall = 0;
+
+BOOL gbWallFilled = FALSE;
+BOOL gbDisplayedFirstWall = FALSE;
+BOOL gbLinesFilled = FALSE;
+BOOL gbAllocateMemoryAgain = FALSE;
+
+void DrawCharacter();
+void RandomCharacter(int RandomNumber);
+
+void DrawRoom();
+void DrawWall();
+void StoreWall();
+
+
+void DrawMatrixLines();
+void StoreLines();
