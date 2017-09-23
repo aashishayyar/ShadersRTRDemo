d4ab4254257b1e5008540b46b2644516444c4ed8
Aashish Ayyar <kartik.iyer30@gmail.com>
2017-09-23 16:12:45 +0530

    Changes to be committed: 	new file:   MAIN/Clock.h 	new file:   MAIN/DoorAndFrame.cpp 	new file:   MAIN/DoorHeader.h 	new file:   MAIN/Main.cpp 	new file:   MAIN/Main.h 	new file:   MAIN/Main_Room.h 	new file:   MAIN/Matrix.h 	new file:   MAIN/Neo.cpp 	new file:   MAIN/Neo.h 	new file:   MAIN/Room.cpp 	new file:   MAIN/Room.h

---
+++ Main/MAIN/Main.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <windows.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+#include <math.h>
+#include <stdio.h>
+
+void MainRoom(void);
+void DrawNeo(void);
+
+void moveNeo(void);
