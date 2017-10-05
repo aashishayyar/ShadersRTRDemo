e5fe9b925112941bb256299356350238be0f274f
Pri_de <pribhasme@gmail.com>
2017-10-06 00:35:15 +0530

    Removed warning from ShaderBullets.cpp

--- Final - 02/Last/ShadersBullets.cpp
+++ Final - 02/Last/ShadersBullets.cpp
@@ -28,140 +28,143 @@ typedef struct bullShaderCords {
 
 // bullets spaced for character 'S'
 bulletShaderData positionS[18] = {
-	{ -4.6, 1.5, -3.0, 0.2 },
-	{ -4.5, 1.7, -1.0, 0.3 },
-	{ -4.8, 2.0, -1.0, 0.25 },
-	{ -4.8, 1.7, -2.5, 0.3 },
-	{ -4.8, 1.6, -0.5, 0.4 },
-	{ -4.7, 1.2, 0.0, 0.3 },
-	{ -5.2, 1.0, -0.4, 0.35 },
-	{ -5.8, 0.7, -1.0, 0.4 },
-	{ -4.9, 0.35, 0.0, 0.3 },
-	{ -5.0, 0.1, -0.5, 0.4 },
-	{ -4.3, -0.1, 0.2, 0.4 },
-	{ -4.6, -0.4, 0.0, 0.45 },
-	{ -5.15, -0.75, -0.7, 0.35 },
-	{ -5.5, -1.0, -1.0, 0.4 },
-	{ -6.4, -1.5, -2.0, 0.4 },
-	{ -6.5, -1.75, -1.7, 0.25 },
-	{ -6.8, -2.0, -1.8, 0.2 },
-	{ -6.45, -2.05, -1.0, 0.15 }
+
+	{ -4.60f, 1.50f, -3.00f, 0.20f },
+	{ -4.50f, 1.70f, -1.00f, 0.30f },
+	{ -4.80f, 2.00f, -1.00f, 0.25f },
+	{ -4.80f, 1.70f, -2.50f, 0.30f },
+	{ -4.80f, 1.60f, -0.50f, 0.40f },
+	{ -4.70f, 1.20f,  0.00f, 0.30f },
+	{ -5.20f, 1.00f, -0.40f, 0.35f },
+	{ -5.80f, 0.70f, -1.00f, 0.40f },
+	{ -4.90f, 0.35f,  0.00f, 0.30f },
+	{ -5.00f, 0.10f, -0.50f, 0.40f },
+	{ -4.30f, -0.10f, 0.20f, 0.40f },
+	{ -4.60f, -0.40f, 0.00f, 0.45f },
+	{ -5.15f, -0.75f, -0.70f, 0.35f },
+	{ -5.50f, -1.00f, -1.00f, 0.40f },
+	{ -6.40f, -1.50f, -2.00f, 0.40f },
+	{ -6.50f, -1.75f, -1.70f, 0.25f },
+	{ -6.80f, -2.00f, -1.80f, 0.20f },
+	{ -6.45f, -2.05f, -1.00f, 0.15f }
+
 };
 
 // bullets spaced for character 'H'
 bulletShaderData positionH[13] = {
-	{ -3.0, 0.65, -1.0, 0.4 },
-	{ -2.5, 0.25, 0.5, 0.3 },
-	{ -2.85, 0.0, -0.5, 0.4 },
-	{ -2.35, -0.3, 1.0, 0.3 },
-	{ -2.8, -0.7, -0.4, 0.35 },
-	{ -2.35, -0.9, 1.0, 0.35 },
-	{ -4.0, 0.65, -1.0, 0.4 },
-	{ -3.4, 0.25, 0.3, 0.3 },
-	{ -3.8, -0.05, -0.5, 0.35 },
-	{ -3.35, -0.3, 0.5, 0.3 },
-	{ -3.75, -0.65, -0.4, 0.35 },
-	{ -3.35, -0.9, 0.5, 0.4 },
-	{ -2.6, -0.2, 1.5, 0.3 }
+	{ -3.00f, 0.65f,-1.00f, 0.40f },
+	{ -2.50f, 0.25f, 0.50f, 0.30f },
+	{ -2.85f, 0.00f,-0.50f, 0.40f },
+	{ -2.35f,-0.30f, 1.00f, 0.30f },
+	{ -2.80f,-0.70f,-0.40f, 0.35f },
+	{ -2.35f,-0.90f, 1.00f, 0.35f },
+	{ -4.00f, 0.65f,-1.00f, 0.40f },
+	{ -3.40f, 0.25f, 0.30f, 0.30f },
+	{ -3.80f,-0.05f,-0.50f, 0.35f },
+	{ -3.35f,-0.30f, 0.50f, 0.30f },
+	{ -3.75f,-0.65f,-0.40f, 0.35f },
+	{ -3.35f,-0.90f, 0.50f, 0.40f },
+	{ -2.60f,-0.20f, 1.50f, 0.30f }
 };
 
 // bullets spaced for character 'A'
 bulletShaderData positionA[15] = {
-	{ -1.0, 0.5, -0.0, 0.35 },
-	{ -1.1, 0.2, -1.5, 0.5 },
-	{ -0.9, -0.2, 0.5, 0.35 },
-	{ -0.9, -0.1, -2.0, 0.2 },
-	{ -0.85f, -0.55, 0.5, 0.35 },
-	{ -1.0, -1.0, -1.0, 0.5 },
-	{ -0.7, -0.9, 1.0, 0.2 },
-	{ -2.5, 0.1, -2.5, 0.5 },
-	{ -1.8, -0.25, 0.5, 0.3 },
-	{ -2.15, -0.65, -1.0, 0.4 },
-	{ -1.65, -0.8, 1.0, 0.3 },
-	{ -2.2, 0.5, -1.5, 0.4 },
-	{ -2.0, 0.8, -3.0, 0.65 },
-	{ -1.5, -0.3, 0.5, 0.3 },
-	{ -1.6, -0.4, -2.5, 0.5 }
+	{ -1.00f,  0.50f, -0.00f, 0.35f },
+	{ -1.10f,  0.20f, -1.50f, 0.50f },
+	{ -0.90f, -0.20f,  0.50f, 0.35f },
+	{ -0.90f, -0.10f, -2.00f, 0.20f },
+	{ -0.85f, -0.55f,  0.50f, 0.35f },
+	{ -1.00f, -1.00f, -1.00f, 0.50f },
+	{ -0.70f, -0.90f,  1.00f, 0.20f },
+	{ -2.50f,  0.10f, -2.50f, 0.50f },
+	{ -1.80f, -0.25f,  0.50f, 0.30f },
+	{ -2.15f, -0.65f, -1.00f, 0.40f },
+	{ -1.65f, -0.80f,  1.00f, 0.30f },
+	{ -2.20f,  0.50f, -1.50f, 0.40f },
+	{ -2.00f,  0.80f, -3.00f, 0.65f },
+	{ -1.50f, -0.30f,  0.50f, 0.30f },
+	{ -1.60f, -0.40f, -2.50f, 0.50f }
 };
 
 // bullets spaced for character 'D'
 bulletShaderData positionD[16] = {
-	{ -0.3, 0.7, -1.2, 0.5 },
-	{ 0.12, 0.6, -0.5, 0.35 },
-	{ 0.45, 0.4, 1.0, 0.4 },
-	{ 0.75, 0.3, 0.0, 0.3 },
-	{ 1.1, 0.0, -2.5, 0.55 },
-	{ 0.8, -0.3, 0.2, 0.3 },
-	{ 0.6, -0.5, 1.8, 0.3 },
-	{ 0.45, -0.85, 0.0, 0.3 },
-	{ 0.2, -1.0, -0.8, 0.45 },
-	{ -0.2, -1.0, 0.0, 0.35 },
-	{ 0.2, -1.0, -0.8, 0.45 },
-	{ 0.2, -1.0, -0.8, 0.45 },
-	{ -0.2, -0.8, -0.0, 0.2 },
-	{ -0.2, -0.4, 2.0, 0.3 },
-	{ -0.25, -0.2, -1.0, 0.4 },
-	{ -0.2, 0.2, 1.0, 0.4 }
+	{ -0.30f, 0.70f,-1.20f, 0.50f },
+	{ 0.12f, 0.60f,-0.50f, 0.35f },
+	{ 0.45f, 0.40f, 1.00f, 0.40f },
+	{ 0.75f, 0.30f, 0.00f, 0.30f },
+	{ 1.10f, 0.00f,-2.50f, 0.55f },
+	{ 0.80f,-0.30f, 0.20f, 0.30f },
+	{ 0.60f,-0.50f, 1.80f, 0.30f },
+	{ 0.45f,-0.85f, 0.00f, 0.30f },
+	{ 0.20f,-1.00f,-0.80f, 0.45f },
+	{ -0.20f,-1.00f, 0.00f, 0.35f },
+	{ 0.20f,-1.00f,-0.80f, 0.45f },
+	{ 0.20f,-1.00f,-0.80f, 0.45f },
+	{ -0.20f,-0.80f,-0.00f, 0.20f },
+	{ -0.20f,-0.40f, 2.00f, 0.30f },
+	{ -0.25f,-0.20f,-1.00f, 0.40f },
+	{ -0.20f, 0.20f, 1.00f, 0.40f }
 };
 
 // bullets spaced for character 'E'
 bulletShaderData positionE[12] = {
-	{ 2.0, 0.7, -2.2, 0.5 },
-	{ 2.0, 0.6, -0.2, 0.3 },
-	{ 2.5, 0.6, -1.0, 0.4 },
-	{ 1.9, 0.3, -2.0, 0.4 },
-	{ 1.35, -0.1, 1.0, 0.35 },
-	{ 1.35, -0.1, 2.5, 0.2 },
-	{ 2.0, -0.1, 1.0, 0.3 },
-	{ 2.1, -0.75, -3.0, 0.55 },
-	{ 1.4, -0.9, 0.3, 0.35 },
-	{ 1.6, -0.8, 0.6, 0.28 },
-	{ 2.0, -0.9, 0.0, 0.3 },
-	{ 3.0, -1.2, -2.8, 0.5 }
+	{ 2.00f,  0.70f, -2.20f, 0.50f },
+	{ 2.00f,  0.60f, -0.20f, 0.30f },
+	{ 2.50f,  0.60f, -1.00f, 0.40f },
+	{ 1.90f,  0.30f, -2.00f, 0.40f },
+	{ 1.35f, -0.10f,  1.00f, 0.35f },
+	{ 1.35f, -0.10f,  2.50f, 0.20f },
+	{ 2.00f, -0.10f,  1.00f, 0.30f },
+	{ 2.10f, -0.75f, -3.00f, 0.55f },
+	{ 1.40f, -0.90f,  0.30f, 0.35f },
+	{ 1.60f, -0.80f,  0.60f, 0.28f },
+	{ 2.00f, -0.90f,  0.00f, 0.30f },
+	{ 3.00f, -1.20f, -2.80f, 0.50f }
 };
 
 // bullets spaced for character 'R'
 bulletShaderData positionR[16] = {
-	{ 3.3, 0.6, -0.8, 0.4 },
-	{ 3.8, 0.6, -1.0, 0.3 },
-	{ 3.8, 0.35, -0.2, 0.3 },
-	{ 4.2f, 0.03, -1.0, 0.3 },
-	{ 3.8f, -0.2, -0.5, 0.3 },
-	{ 3.9, -0.4, -1.5, 0.3 },
-	{ 4.4, -0.7, -2.0, 0.35 },
-	{ 3.7, -0.85, 0.0, 0.3 },
-	{ 4.5, -1.3, -1.3, 0.25 },
-	{ 2.4, 0.15, 1.5, 0.25 },
-	{ 3.5, -0.1, -1.5, 0.4 },
-	{ 2.0, -0.25, 2.5, 0.2 },
-	{ 3.7, -0.8, -2.0, 0.4 },
-	{ 2.95, -0.95, 0.0, 0.35 },
-	{ 3.5, -1.18, 1.0, 0.18 },
-	{ 5.1, -1.9, -2.0, 0.2 }
+	{ 3.30f,  0.60f, -0.80f, 0.40f },
+	{ 3.80f,  0.60f, -1.00f, 0.30f },
+	{ 3.80f,  0.35f, -0.20f, 0.30f },
+	{ 4.20f,  0.03f, -1.00f, 0.30f },
+	{ 3.80f, -0.20f, -0.50f, 0.30f },
+	{ 3.90f, -0.40f, -1.50f, 0.30f },
+	{ 4.40f, -0.70f, -2.00f, 0.35f },
+	{ 3.70f, -0.85f,  0.00f, 0.30f },
+	{ 4.50f, -1.30f, -1.30f, 0.25f },
+	{ 2.40f,  0.15f,  1.50f, 0.25f },
+	{ 3.50f, -0.10f, -1.50f, 0.40f },
+	{ 2.00f, -0.25f,  2.50f, 0.20f },
+	{ 3.70f, -0.80f, -2.00f, 0.40f },
+	{ 2.95f, -0.95f,  0.00f, 0.35f },
+	{ 3.50f, -1.18f,  1.00f, 0.18f },
+	{ 5.10f, -1.90f, -2.00f, 0.20f }
 };
 
 // bullets spaced for character 's'
 bulletShaderData positionLastS[11] = {
-	{ 5.9, 0.5, -0.8, 0.2 },
-	{ 5.5, 0.6, -0.6, 0.3 },
-	{ 4.8, 0.52, 0.0, 0.3 },
-	{ 2.8, 0.22, 3.0, 0.2 },
-	{ 4.8, 0.05, -0.5, 0.35 },
-	{ 5.1, -0.1, -0.6, 0.4 },
-	{ 5.5, -0.3, -0.4, 0.3 },
-	{ 4.0, -0.43, 2.0, 0.2 },
-	{ 6.75, -1.1, -2.5, 0.4 },
-	{ 5.2, -1.1, -0.6, 0.3 },
-	{ 4.0, -0.95, 1.0, 0.2 }
+	{ 5.90f,  0.50f, -0.80f, 0.20f },
+	{ 5.50f,  0.60f, -0.60f, 0.30f },
+	{ 4.80f,  0.52f,  0.00f, 0.30f },
+	{ 2.80f,  0.22f,  3.00f, 0.20f },
+	{ 4.80f,  0.05f, -0.50f, 0.35f },
+	{ 5.10f, -0.10f, -0.60f, 0.40f },
+	{ 5.50f, -0.30f, -0.40f, 0.30f },
+	{ 4.00f, -0.43f,  2.00f, 0.20f },
+	{ 6.75f, -1.10f, -2.50f, 0.40f },
+	{ 5.20f, -1.10f, -0.60f, 0.30f },
+	{ 4.00f, -0.95f,  1.00f, 0.20f }
 };
 
 
-// structure for bullets shade
-typedef struct {
-	float rColor;
-	float gColor;
-	float bColor;
-};
+
+//// structure for bullets shade
+//typedef struct {
+//	float rColor;
+//	float gColor;
+//	float bColor;
+//};
 
 
 
@@ -285,73 +288,73 @@ void drawShadersBullet(float xCord, float yCord, float zCord, float bulletScale)
 	//glTranslatef(0.0, -0.5, 0.0); 
 
 	glPushMatrix(); // save position of first object which will rotate around the initial point
-	glColor3f(160.0 / 255.0, 82.0 / 255.0, 45.0 / 255.0);
+	glColor3f(160.0f / 255.0f, 82.0f / 255.0f, 45.0f / 255.0f);
 	quadricBullet = gluNewQuadric();
 
 	// inner disk back of the bullet
-	gluDisk(quadricBullet, 0.001, 0.35*bulletScale, 20, 20);
+	gluDisk(quadricBullet, 0.001f, 0.35f*bulletScale, 20, 20);
 
 	//yellowinsh shade
-	glColor3f(218.0 / 255.0, 168.0 / 255.0, 32.0 / 255.0);
+	glColor3f(218.0f / 255.0f, 168.0f / 255.0f, 32.0f / 255.0f);
 	quadricBullet = gluNewQuadric();
 
 	// outer disk back of the bullet
-	gluDisk(quadricBullet, 0.35*bulletScale, 0.5*bulletScale, 20, 20);
+	gluDisk(quadricBullet, 0.35f*bulletScale, 0.5f*bulletScale, 20, 20);
 
 	// now we will draw second object which is related to the first one
 	// back cylinder
 	glPopMatrix(); // actually not needed since we didn't change the position
 
 	glPushMatrix(); // save the previous position
-	glColor3f(218.0 / 255.0, 168.0 / 255.0, 32.0 / 255.0);
+	glColor3f(218.0f / 255.0f, 168.0f / 255.0f, 32.0f / 255.0f);
 	quadricBullet = gluNewQuadric();
-	gluCylinder(quadricBullet, 0.5*bulletScale, 0.5*bulletScale, 0.08*bulletScale, 20, 20);
+	gluCylinder(quadricBullet, 0.5f*bulletScale, 0.5f*bulletScale, 0.08f*bulletScale, 20, 20);
 	glPopMatrix();
 
-	glTranslatef(0.0, 0.0, 0.08*bulletScale); // since we have rotated the whole matrix by hard
+	glTranslatef(0.0, 0.0, 0.08f*bulletScale); // since we have rotated the whole matrix by hard
 	// coded 90 degrees first, so every axis got shifted and +z takes place of +x
 	//glRotatef((GLfloat)extra1, 0.0, 0.0, 1.0);
 	//glTranslatef(0.0, -0.5, 0.0);
 
 	glPushMatrix();
-	glColor3f(184.0 / 255.0, 134.0 / 255.0, 11.0 / 255.0);
+	glColor3f(184.0f / 255.0f, 134.0f / 255.0f, 11.0f / 255.0f);
 	quadricBullet = gluNewQuadric();
-	gluDisk(quadricBullet, 0.01, 0.5*bulletScale, 20, 20);
+	gluDisk(quadricBullet, 0.01f, 0.5f*bulletScale, 20, 20);
 
 	glPopMatrix(); // translated position by 0.05
 
 	glTranslatef(0.0, 0.0, 0.0);
 	glPushMatrix(); // save the previous position second cylinder
-	glColor3f(218.0 / 255.0, 168.0 / 255.0, 32.0 / 255.0);
+	glColor3f(218.0f / 255.0f, 168.0f / 255.0f, 32.0f / 255.0f);
 	quadricBullet = gluNewQuadric();
-	gluCylinder(quadricBullet, 0.4*bulletScale, 0.4*bulletScale, 0.15*bulletScale, 20, 20);
+	gluCylinder(quadricBullet, 0.4f*bulletScale, 0.4f*bulletScale, 0.15f*bulletScale, 20, 20);
 
 	glPopMatrix();
 
-	glTranslatef(0.0, 0.0, 0.15*bulletScale);
+	glTranslatef(0.0, 0.0, 0.15f*bulletScale);
 	glPushMatrix(); 
-	glColor3f(184.0 / 255.0, 134.0 / 255.0, 11.0 / 255.0);
+	glColor3f(184.0f / 255.0f, 134.0f / 255.0f, 11.0f / 255.0f);
 	//glColor3f(0.6, 0.2, 0.0);
 	quadricBullet = gluNewQuadric();
-	gluDisk(quadricBullet, 0.01, 0.5*bulletScale, 20, 20);
+	gluDisk(quadricBullet, 0.01f, 0.5f*bulletScale, 20, 20);
 
 	glPopMatrix();
 
 	//glTranslatef(0.0, 0.0, 0.5);
 	glPushMatrix();
-	glColor3f(0.9, 0.7, 0.2);
+	glColor3f(0.9f, 0.7f, 0.2f);
 	quadricBullet = gluNewQuadric();
-	gluCylinder(quadricBullet, 0.5*bulletScale, 0.5*bulletScale, 1.3*bulletScale, 20, 20);
+	gluCylinder(quadricBullet, 0.5f*bulletScale, 0.5f*bulletScale, 1.3f*bulletScale, 20, 20);
 
 	glPopMatrix();
 
 	//glRotatef(90, 1.0, 0.0, 0.0);
-	glTranslatef(0.0, 0.0, 1.3*bulletScale);
+	glTranslatef(0.0, 0.0, 1.3f*bulletScale);
 	glPushMatrix(); // save the previous position second cylinder
-	glScalef(1.0, 1.0, 2.0);
-	glColor3f(210.0 / 255.0, 105.0 / 255.0, 30.0 / 255.0);
+	glScalef(1.0f, 1.0f, 2.0f);
+	glColor3f(210.0f / 255.0f, 105.0f / 255.0f, 30.0f / 255.0f);
 	quadricBullet = gluNewQuadric();
-	gluSphere(quadricBullet, 0.5*bulletScale, 20, 20);
+	gluSphere(quadricBullet, 0.5f*bulletScale, 20, 20);
 
 	glPopMatrix();
 	glPopMatrix();
